b500fba729c879cf322ba5964ae4973262eaaf8a
abdelhamid ameziane <ameziane001@gmail.com>
2024-11-27 15:34:12 +0100

    exo rails yelp mvp completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,11 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+Restaurant.destroy_all
+
+Restaurant.create(name: "Epicure", category: "french", address: "14 rue lewagon, Paris, 75001", phone_number:"+33456763829")
+Restaurant.create(name: "Sakura Sushi", category: "japanese", address: "8 Chome-10 Ginza, Tokyo, 104-0061", phone_number:"+33456763829" )
+Restaurant.create(name: "La Trattoria", category: "italian", address: "21 Via Roma, Milan, 20121", phone_number:"+33456763829")
+Restaurant.create(name: "Wok'n Roll", category: "chinese", address: "88 Chinatown Blvd, San Francisco, CA 94108", phone_number:"+33456763829")
+Restaurant.create(name: "The Belgian Waffle House", category: "belgian", address: "15 Grand Place, Brussels, 1000", phone_number:"+33456763829")
